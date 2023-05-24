47935abe1cce1b815496a71be2ad147721f3f7f7
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-23 21:54:37 -0300

    feat: front uses machineLearning.py

--- app.py
+++ app.py
@@ -1,126 +1,133 @@
-import base64, pandas as pd, dash
-from io import StringIO
-from dash import Dash, dcc, html, dash_table
-import plotly.express as px
+import base64
+import pandas as pd
+import dash
+from dash import html
+import dash_table
+from dash import dcc
+import plotly.graph_objects as go
+from matplotlib import pyplot as plt
 
-from Scripts.MachineLearning import AcharAnomalias
+from io import StringIO
+from Scripts.MachineLearning import criar_grafico_e_tabela
 
 # Criar o aplicativo Dash
-app = Dash(__name__)
+app = dash.Dash(__name__)
+
+# Estilos CSS personalizados
+styles = {
+    'upload': {
+        'width': '99%',
+        'height': '60px',
+        'lineHeight': '60px',
+        'borderWidth': '1px',
+        'borderStyle': 'dashed',
+        'borderRadius': '5px',
+        'textAlign': 'center',
+        'margin': '10px',
+        'background-color': '#f0f0f0',
+        'color': '#555',
+        'cursor': 'pointer',
+    },
+    'button': {
+        'margin': '20px auto',
+        'display': 'block',
+        'font-size': '15px',
+        'background-color': '#007bff',
+        'color': '#fff',
+        'border': 'none',
+        'border-radius': '5px',
+        'padding': '10px 20px',
+        'cursor': 'pointer',
+    },
+    'table-container': {
+        'maxHeight': '400px',
+        'overflowY': 'scroll',
+        'border': '1px solid #ccc',
+        'border-radius': '5px',
+    },
+    'graph': {
+        'margin': '20px 0',
+    },
+}
 
 # Layout do aplicativo
 app.layout = html.Div([
-    html.H1("Análise de dados"),
+    html.H1("Análise de dados", style={'text-align': 'center', 'margin-top': '30px'}),
+
     dcc.Upload(
         id='upload-data',
         children=html.Div([
             'Arraste e solte ou ',
             html.A('selecione um arquivo')
         ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Permitir upload de apenas um arquivo
+        style=styles['upload'],
         multiple=False
     ),
-    html.Button('Processar Arquivo', id='process-data-button', n_clicks=0),
-    # Tabela para exibir os dados inválidos
-    dash_table.DataTable(
-        id='invalid-data-table',
-        columns=[{"name": i, "id": i} for i in ['Eletrodomestico', 'Comodo', 'Gasto', 'Data', 'MediaGasto', 'DesvioPadrao']],
-        data=[],
-        style_table={
-            'maxHeight': '400px',
-            'overflowY': 'scroll'
-        }
-    ),
-    # Gráfico para exibir os dados processados
-    dcc.Graph(id='processed-data-graph'),
 
-    # Checkbox para filtrar os dados exibidos no gráfico
-    dcc.Checklist(
-        id='filter-data-checklist',
-        options=[
-            {'label': 'Mostrar apenas dados processados', 'value': 'processed'}
-        ],
-        value=[]
-    )
+    html.Button('Processar Arquivo', id='process-data-button', n_clicks=0, style=styles['button']),
+
+    html.Div(id='upload-feedback'),
+
+    html.Div([
+        html.Div(id='table-container', style=styles['table-container'], children=[
+            dash_table.DataTable(
+                id='invalid-data-table',
+                columns=[{"name": i, "id": i} for i in ['Barn [kW]','Dishwasher [kW]','Fridge [kW]','Furnace 1 [kW]','Furnace 2 [kW]','Garage door [kW]','Home office [kW]','House overall [kW]','Kitchen 12 [kW]','Kitchen 14 [kW]','Kitchen 38 [kW]','Living room [kW]','Microwave [kW]','Solar [kW]','Well [kW]','Wine cellar [kW]','apparentTemperature','datetime','dewPoint','gen [kW]','humidity','precipIntensity','precipProbability','pressure','temperature','use [kW]','visibility','windBearing','windSpeed','anomaly']],
+                data=[],
+            )
+        ])
+    ]),
+
+    html.Div(id='processed-data-graph', style=styles['graph']),
 ])
 
-# Variável para armazenar os dados processados
-processed_data = None
-dataframe = None
-
-# Callback para processar o arquivo e exibir os dados inválidos na tabela
-@app.callback(
-    dash.dependencies.Output('invalid-data-table', 'data'),
-    dash.dependencies.Input('process-data-button', 'n_clicks'),
-    dash.dependencies.State('upload-data', 'contents')
-)
-
-def process_file(n_clicks, contents):
-    global processed_data, dataframe
-    
+# Função para processar o arquivo, exibir os dados inválidos na tabela e exibir os gráficos
+def process_and_display_data(n_clicks, contents):
     if n_clicks > 0 and contents is not None:
         content_type, content_string = contents.split(',')
         decoded = base64.b64decode(content_string)
         try:
-            dataframe = pd.read_csv(StringIO(decoded.decode('utf-8')), delimiter=";")
-            processed_data = AcharAnomalias(dataframe)
-            invalid_data = processed_data.to_dict('records')
-            return invalid_data
-        
+            dados = criar_grafico_e_tabela(StringIO(decoded.decode('utf-8')))
+
+            dataframe = dados['anomalias'].to_dict('records')
+            graficos = dados['graficos']
+
+            feedback = html.Div('Arquivo processado com sucesso!', style={'color': 'green'})
+
+            # Exibir os gráficos com base nos dados processados
+            graphs = []
+            for i, grafico in enumerate(graficos):
+                if isinstance(grafico, plt.Figure):
+                    fig = go.Figure()
+                    for ax in grafico.get_axes():
+                        for line in ax.get_lines():
+                            fig.add_trace(go.Scatter(x=line.get_xdata(), y=line.get_ydata(), name=line.get_label()))
+                    fig.update_layout(
+                        title=f'Gráfico {i+1}',
+                        xaxis_title='Data',
+                        yaxis_title='Gasto'
+                    )
+                    graphs.append(dcc.Graph(id=f'processed-data-graph-{i}', figure=fig, style=styles['graph']))
+
+            return dataframe, feedback, graphs
+
         except Exception as e:
-            print(e)
-            return []
+            feedback = html.Div(f'Ocorreu um erro ao processar o arquivo: {str(e)}', style={'color': 'red'})
+            return [], feedback, None
     else:
-        return []
+        return [], None, None
 
-
-# Callback para exibir o gráfico com base nos dados processados
+# Callback para processar o arquivo, exibir os dados inválidos na tabela e exibir os gráficos
 @app.callback(
-    dash.dependencies.Output('processed-data-graph', 'figure'),
-    dash.dependencies.Input('process-data-button', 'n_clicks'),
-    dash.dependencies.State('upload-data', 'contents'),
-    dash.dependencies.State('filter-data-checklist', 'value')
+    [dash.dependencies.Output('invalid-data-table', 'data'),
+     dash.dependencies.Output('upload-feedback', 'children'),
+     dash.dependencies.Output('processed-data-graph', 'children')],
+    [dash.dependencies.Input('process-data-button', 'n_clicks')],
+    [dash.dependencies.State('upload-data', 'contents')]
 )
-
-def display_graph(n_clicks, contents, filter_value):
-    global processed_data, dataframe
-
-    if n_clicks > 0 and contents is not None:
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-
-        try:
-            dataframe = pd.read_csv(StringIO(decoded.decode('utf-8')), delimiter=";")
-            processed_data = AcharAnomalias(dataframe)
-
-            # Obter os dados que não estão em processed_data e plotar em verde
-            df_not_processed = dataframe[~dataframe.set_index(['Comodo', 'Data', 'Eletrodomestico']).index.isin(processed_data.set_index(['Comodo', 'Data', 'Eletrodomestico']).index)]
-            fig = px.line(df_not_processed, x='Data', y='Gasto', color='Comodo', line_group='Eletrodomestico', labels={'Gasto': 'Gasto original'})
-
-            # Obter os dados que estão em processed_data e plotar em vermelho, separados por comodo
-            for comodo in processed_data['Comodo'].unique():
-                df_comodo = processed_data[processed_data['Comodo'] == comodo]
-                fig.add_trace(px.line(df_comodo, x='Data', y='Gasto', color=px.Constant(comodo), line_group='Eletrodomestico',
-                                       labels={'Gasto': 'Gasto processado'})['data'][0])
-
-            fig.update_layout(title='Gastos por Comodo', xaxis_title='Data', yaxis_title='Gasto')
-            return fig
-        except Exception as e:
-            print(e)
-            return {}
-    else:
-        return {}
-
+def process_and_display_callback(n_clicks, contents):
+    dataframe, feedback, graphs = process_and_display_data(n_clicks, contents)
+    return dataframe, feedback, graphs
 
 if __name__ == '__main__':
     app.run_server(debug=True)
